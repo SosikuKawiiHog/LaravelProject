dfd7f8695765f5e98560827e3cfbd6ff62788dca
SosikuKawiiHog <razdobarov_se@edu.surgu.ru>
2026-02-07 16:29:26 +0500

    Установка Sanctum

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,14 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\GameControllerApi;
+use App\Http\Controllers\ReviewController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DeveloperControllerApi;
 use App\Http\Controllers\ReviewControllerApi;
 
+Route::post('/login', [AuthController::class, 'login']);
 
 Route::get('/game',[GameControllerApi::class, 'index']);
 Route::get('/game/{id}',[GameControllerApi::class, 'show']);
@@ -13,5 +16,18 @@ Route::get('/game/{id}',[GameControllerApi::class, 'show']);
 Route::get('/developer',[DeveloperControllerApi::class, 'index']);
 Route::get('/developer/{id}',[DeveloperControllerApi::class, 'show']);
 
-Route::get('/reviews',[ReviewControllerApi::class, 'index']);
-Route::get('/reviews/{id}',[ReviewControllerApi::class, 'show']);
+//Route::get('/reviews',[ReviewControllerApi::class, 'index']);
+//Route::get('/reviews/{id}',[ReviewControllerApi::class, 'show']);
+
+
+Route::middleware(['auth:sanctum'])->get('/logout', [AuthController::class, 'logout']);
+
+Route::group(['middleware'=>['auth:sanctum']], function () {
+    Route::get('/reviews',[ReviewControllerApi::class, 'index']);
+    Route::get('/reviews/{id}',[ReviewControllerApi::class, 'show']);
+
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+    Route::get('/logout', [AuthController::class, 'logout']);
+});
